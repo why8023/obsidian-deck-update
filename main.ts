cb924d02eb8f46e5439bf715fa6fae2d514cb216
whying <291424708@qq.com>
2025-09-15 13:47:23 +0800

    ✨ feat(manifest, package, main): 更新插件为Deck Auto Updater
    - 将插件名称更改为“Deck Auto Updater”，并更新ID和描述
    - 实现文件创建和重命名时自动更新TARGET DECK前置数据
    - 添加设置选项以包含文件名在路径中
    - 更新版本号为0.1.0

--- main.ts
+++ main.ts
@@ -1,134 +1,87 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DeckUpdaterSettings {
+  includeFilename: boolean; // true -> include filename in deck path
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: DeckUpdaterSettings = {
+  includeFilename: false,
+};
+
+export default class DeckAutoUpdater extends Plugin {
+  settings: DeckUpdaterSettings;
+
+  async onload() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+
+    // Handle new file creation
+    this.registerEvent(
+      this.app.vault.on('create', async (file) => {
+        if (!(file instanceof TFile) || file.extension !== 'md') return;
+        await this.updateDeckFrontmatter(file);
+      })
+    );
+
+    // Handle move/rename
+    this.registerEvent(
+      this.app.vault.on('rename', async (file) => {
+        if (!(file instanceof TFile) || file.extension !== 'md') return;
+        await this.updateDeckFrontmatter(file);
+      })
+    );
+
+    this.addSettingTab(new DeckUpdaterSettingTab(this.app, this));
+  }
+
+  private buildDeckPath(file: TFile): string {
+    // Normalize path to forward slashes and strip .md
+    const rel = file.path.replace(/\\/g, '/').replace(/\.md$/i, '');
+    const parts = rel.split('/').filter(Boolean);
+
+    const effectiveParts = this.settings.includeFilename
+      ? parts
+      : parts.slice(0, -1); // folders only
+
+    const base = 'obsidian_sync';
+    return effectiveParts.length ? `${base}::${effectiveParts.join('::')}` : base;
+  }
+
+  private async updateDeckFrontmatter(file: TFile) {
+    const deck = this.buildDeckPath(file);
+    await this.app.fileManager.processFrontMatter(file, (fm) => {
+      fm['TARGET DECK'] = deck;
+    });
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class DeckUpdaterSettingTab extends PluginSettingTab {
+  plugin: DeckAutoUpdater;
 
-	async onload() {
-		await this.loadSettings();
+  constructor(app: App, plugin: DeckAutoUpdater) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName('Include filename in deck path')
+      .setDesc(
+        'On: obsidian_sync::<folders>::<filename>. Off: obsidian_sync::<folders>'
+      )
+      .addToggle((toggle) =>
+        toggle
+          .setValue(this.plugin.settings.includeFilename)
+          .onChange(async (value) => {
+            this.plugin.settings.includeFilename = value;
+            await this.plugin.saveSettings();
+          })
+      );
+  }
 }
